b88163fcf29d382c909b5b37af6261f72650f1b0
gustavool <gustavool@live.com>
2022-09-24 18:38:17 -0300

    feat: connection mongodb

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,12 @@
 import express from "express";
+import mongoose from "mongoose";
+import "dotenv/config";
 
 const app = express();
+
+mongoose.connect(`${process.env.DATABASE_URL}`, () =>
+  console.log("📦 Connected to database")
+);
 
 app.listen(3000, () =>
   console.log("🔥 Server is running at http://localhost:3000")
